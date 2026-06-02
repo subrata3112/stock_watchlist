4f9c4db0bde361ed2a03584fe81285eb090bc84c
subrata3112 <subratob.biswas3213@gmail.com>
2026-06-02 21:10:13 +0530

    Fixed test name

--- src/test/java/com/watchlist/stock_tracker/EquityStockTrackerApplicationTests.java
+++ src/test/java/com/watchlist/stock_tracker/EquityStockTrackerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EquityStockTrackerApplicationTests {
+class StockTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
